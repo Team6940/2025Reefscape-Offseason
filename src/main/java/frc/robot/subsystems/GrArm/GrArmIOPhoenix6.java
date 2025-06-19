0200402bf1d597763c45660874da9433173a790d
Jason2706 <jason20090724@qq.com>
2025-06-19 20:21:18 +0800

    GrArm Subsystem
    GrArm Subsystem

--- src/main/java/frc/robot/subsystems/GrArm/GrArmIOPhoenix6.java
+++ src/main/java/frc/robot/subsystems/GrArm/GrArmIOPhoenix6.java
@@ -1,5 +1,71 @@
 package frc.robot.subsystems.GrArm;
 
-public class GrArmIOPhoenix6 {
-    
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import frc.robot.Constants.GrArmConstants;
+
+public class GrArmIOPhoenix6 implements GrArmIO {
+    private static TalonFX motor;
+
+    private static MotionMagicVoltage m_request = new MotionMagicVoltage(0.);
+    // private static VelocityVoltage m_request=new VelocityVoltage(0);
+
+    public GrArmIOPhoenix6() {
+        motorConfig();
+    }
+
+    private void motorConfig() {
+        motor = new TalonFX(GrArmConstants.GrArmMotorID, "rio");
+        TalonFXConfiguration config = new TalonFXConfiguration();
+        config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        config.Feedback.SensorToMechanismRatio = GrArmConstants.GrArmRatio;
+        config.Voltage.PeakForwardVoltage = 12.0;
+        config.Voltage.PeakReverseVoltage = -12.0;
+        config.Slot0.kP = GrArmConstants.kP;
+        config.Slot0.kI = GrArmConstants.kI;
+        config.Slot0.kD = GrArmConstants.kD;
+        config.Slot0.kV = GrArmConstants.kV;
+        config.Slot0.kS = GrArmConstants.kS;
+        config.Slot0.kG = GrArmConstants.kG;
+
+        config.MotorOutput.Inverted = GrArmConstants.Inverted;
+
+        config.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+
+        config.MotionMagic.MotionMagicCruiseVelocity = GrArmConstants.MaxVelocity;
+        config.MotionMagic.MotionMagicAcceleration = GrArmConstants.Acceleration;
+
+        motor.getConfigurator().apply(config);
+
+        zeroGrArmPostion();
+    }
+
+    @Override
+    public void setVoltage(double voltage) {
+        motor.setVoltage(voltage);
+    }
+
+    @Override
+    public void setPosition(double position) {
+        if (position == 0) {
+            motor.stopMotor();
+        }
+        motor.setControl(m_request.withPosition(position * GrArmConstants.ArmRatio));
+    }
+
+    public void updateInputs(GrArmIOInputs GrArmInputs) {
+        GrArmInputs.GrArmMotorConnected = BaseStatusSignal.refreshAll(
+                motor.getMotorVoltage(),
+                motor.getSupplyCurrent(),
+                motor.getVelocity()).isOK();
+
+        GrArmInputs.GrArmVoltageVolts = motor.getMotorVoltage().getValueAsDouble();
+        GrArmInputs.GrArmCurrentAmps = motor.getSupplyCurrent().getValueAsDouble();
+
+    }
+
 }
