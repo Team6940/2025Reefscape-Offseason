8ba6714b589bbfc4d5fba28d327d22cb86d5f79f
William Xu <xu_qiran_william@163.com>
2025-08-29 21:37:10 +0800

    2025/8/29

--- src/main/java/frc/robot/commands/GroundIntakeCommands/NewCoralAlignSequence.java
+++ src/main/java/frc/robot/commands/GroundIntakeCommands/NewCoralAlignSequence.java
@@ -80,9 +80,10 @@ public class NewCoralAlignSequence extends Command {
         if (!driverController.getButton(Button.kRightTrigger)) {
             indexer.setLeftRPS(-4.);
             indexer.setRghtRPS(-8.);
-        } else
+        } else {
             indexer.setLeftRPS(4.);
             indexer.setRghtRPS(15.);
+        }
         if ((indexer.getIndexerState() == IndexerState.READY ||
                 driverController.getButton(m_toggleButton)) && arm.isAtTargetPositon()) {
             // if (driverController.getButton(m_toggleButton)) {
